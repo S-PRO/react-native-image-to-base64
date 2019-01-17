5a61c9720950505f1ce823caf2046a4eadcd9f12
AsovaS-pro <44199920+AsovaS-pro@users.noreply.github.com>
2019-01-17 10:08:52 +0200

    Update RNImageToBase64.m

--- ios/RNImageToBase64.m
+++ ios/RNImageToBase64.m
@@ -1,6 +1,7 @@
 #import "RNImageToBase64.h"
 #import <AssetsLibrary/AssetsLibrary.h>
 #import <UIKit/UIKit.h>
+#import <ImageIO/ImageIO.h>
 
 @implementation RNImageToBase64
 
@@ -8,18 +9,56 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(getBase64String:(NSString *)input callback:(RCTResponseSenderBlock)callback)
 {
-  NSURL *url = [[NSURL alloc] initWithString:input];
-  ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-  [library assetForURL:url resultBlock:^(ALAsset *asset) {
-    ALAssetRepresentation *rep = [asset defaultRepresentation];
-    CGImageRef imageRef = [rep fullScreenImage];
-    NSData *imageData = UIImagePNGRepresentation([UIImage imageWithCGImage:imageRef]);
-    NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
-    callback(@[[NSNull null], base64Encoded]);
-  } failureBlock:^(NSError *error) {
-    NSLog(@"that didn't work %@", error);
-    callback(@[error]);
-  }];
+  CGImageRef image = MyCreateCGImageFromFile(input);
+  NSData *imageData = UIImagePNGRepresentation([UIImage imageWithCGImage:image]);
+  NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
+  callback(@[[NSNull null], base64Encoded]);
+}
+
+CGImageRef MyCreateCGImageFromFile (NSString* path)
+{
+    // Get the URL for the pathname passed to the function.
+    NSURL *url = [NSURL fileURLWithPath:path];
+    CGImageRef        myImage = NULL;
+    CGImageSourceRef  myImageSource;
+    CFDictionaryRef   myOptions = NULL;
+    CFStringRef       myKeys[2];
+    CFTypeRef         myValues[2];
+
+    // Set up options if you want them. The options here are for
+    // caching the image in a decoded form and for using floating-point
+    // values if the image format supports them.
+    myKeys[0] = kCGImageSourceShouldCache;
+    myValues[0] = (CFTypeRef)kCFBooleanTrue;
+    myKeys[1] = kCGImageSourceShouldAllowFloat;
+    myValues[1] = (CFTypeRef)kCFBooleanTrue;
+    // Create the dictionary
+    myOptions = CFDictionaryCreate(NULL, (const void **) myKeys,
+                                   (const void **) myValues, 2,
+                                   &kCFTypeDictionaryKeyCallBacks,
+                                   & kCFTypeDictionaryValueCallBacks);
+    // Create an image source from the URL.
+    myImageSource = CGImageSourceCreateWithURL((CFURLRef)url, myOptions);
+    CFRelease(myOptions);
+    // Make sure the image source exists before continuing
+    if (myImageSource == NULL){
+        fprintf(stderr, "Image source is NULL.");
+        return  NULL;
+    }
+    // Create an image from the first item in the image source.
+    myImage = CGImageSourceCreateImageAtIndex(myImageSource,
+                                              0,
+                                              NULL);
+
+    CFRelease(myImageSource);
+    // Make sure the image exists before continuing
+    if (myImage == NULL){
+        fprintf(stderr, "Image not created from image source.");
+        return NULL;
+    }
+
+    return myImage;
 }
 
 @end
+
